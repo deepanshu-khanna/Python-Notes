e4b4871d16b01fb0850908c2820ea48d5553f7c8
Deepanshu Khanna <deepanshukhanna_ae20a16_04@dtu.ac.in>
2023-01-17 19:12:00 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,13 @@
+import os # importing the os module 
+# MODULES: A module is a file written by someone else, which can be imported and used by us in our programs(e.g. OS, tensorflow......)
+
+# PRINT HELLO WORLD IN PYTHON
+
+print("Hello world")
+
+# Print the contents of a directory
+print(os.listdir)
+
+# VARIABLES AND DATATYPES
+
+
